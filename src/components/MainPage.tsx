8b31947587e8c9e9e3016e992c7d872859d8eb0a
cthacker-udel <cthacker@udel.edu>
2021-10-09 11:29:07 -0400

    Added base containers

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+// 1 for generating random number, text box to set first value, text box to set second value, third text box to store result, badge header
+
+export const MainPage = () => {
+
+	return(
+		<>
+			
+		</>
+	);
+
+}
